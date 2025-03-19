ffa32b561a5b57e7f486543c54940c23b42abc7b
Achref-ab <md5539584@gmail.com>
2025-03-18 20:39:21 -0700

    Update calculate.php

--- tp-2/calculate.php
+++ tp-2/calculate.php
@@ -1,29 +1,27 @@
 <?php
-header('Content-Type: application/json');
-
 if (isset($_POST['name'], $_POST['weight'], $_POST['height'])) {
     $name = htmlspecialchars($_POST['name']);
     $weight = floatval($_POST['weight']);
     $height = floatval($_POST['height']);
 
     if ($weight <= 0 || $height <= 0) {
-        echo json_encode(['success' => false, 'message' => 'Invalid input values.']);
+        echo "Invalid input values.";
         exit;
     }
 
     $bmi = $weight / ($height * $height);
-    $interpretation = ($bmi < 18.5) ? "Underweight" :
-                      ($bmi < 25) ? "Normal weight" :
-                      ($bmi < 30) ? "Overweight" : "Obesity";
+    if ($bmi < 18.5) {
+        $interpretation = "Underweight";
+    } elseif ($bmi < 25) {
+        $interpretation = "Normal weight";
+    } elseif ($bmi < 30) {
+        $interpretation = "Overweight";
+    } else {
+        $interpretation = "Obesity";
+    }
 
-    echo json_encode([
-        'success' => true,
-        'bmi' => $bmi,
-        'message' => "Hello, $name. Your BMI is " . number_format($bmi, 2) . " ($interpretation)."
-    ]);
-    exit;
+    echo "Hello, $name. Your BMI is " . number_format($bmi, 2) . " ($interpretation).";
+} else {
+    echo "Data not received.";
 }
-
-echo json_encode(['success' => false, 'message' => 'Data not received.']);
-exit;
 ?>
